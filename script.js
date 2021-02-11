7e303bb901a9b907f8b9beb37b5bc26ff089e3b8
Asif Mahmud <asifmahmud3472@gmail.com>
2021-02-11 13:03:35 +0600

    adding files

---
+++ script.js
@@ -0,0 +1,54 @@
+
+
+function takeInput(){
+    let userInput = document.getElementById('userInput').value;
+    if(userInput == null || userInput == undefined || userInput == "")
+        alert("Please enter a name");
+    else{
+        getData(userInput);
+    }
+}
+
+function getData(userInput){
+    let url = `https://api.lyrics.ovh/suggest/${userInput}`;
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displayData(data));
+}
+
+function displayData(data){
+    document.getElementById('lyrics').innerText = "";
+    let html = "";
+    let l = data['data'].length;
+    for(let i = 0; i < l; i++){
+        html = html+`<div class="search-result col-md-8 mx-auto py-4">
+                        <div class="single-result row align-items-center my-3 p-3">
+                            <div class="col-md-9">
+                                <h3 class="lyrics-name">${data['data'][i].title}</h3>
+                                <p class="author lead">Album by <span>${data['data'][i].artist.name}</span></p>
+                            </div>
+                            <div class="col-md-3 text-md-right text-center">
+                                <button onclick="getLyrics('${data['data'][i].artist.name}', '${data['data'][i].title}')" class="btn btn-success">Get Lyrics</button>
+                            </div>
+                        </div>
+                    </div>`;
+    }
+    document.getElementById('songs').innerHTML = html;
+}
+
+function getLyrics(artist, title){
+    let url = `https://api.lyrics.ovh/v1/${artist}/${title}`;
+    fetch(url)
+    .then(res => res.json())
+    .then(data => showLyrics(data));
+}
+
+function showLyrics(data){
+    if(data.lyrics == null || data.lyrics == "" || data.lyrics == undefined){
+        document.getElementById('lyrics').innerText = "No Lyrics Found";  
+    }
+    else{
+        document.getElementById('lyrics').innerText = data.lyrics;
+    }
+    document.getElementById('lyrics').scrollIntoView();
+}
